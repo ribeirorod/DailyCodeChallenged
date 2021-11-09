0fcf7603582d3b1a6e53534bdf73b0b4fd038bb3
igorturturro <igor_tp@hotmail.com>
2021-11-09 22:48:45 +0100

    First part 003

--- javascript/003.js
+++ javascript/003.js
@@ -1,9 +1,6 @@
 const factorial = (num) => {
-  if (num === 0) {
-    return 1;
-  } else {
-    return num * factorial(num - 1);
-  }
+  const result = num === 0 ? 1 : num * factorial(num - 1);
+  return result;
 };
 
 const fn = (N) => {
